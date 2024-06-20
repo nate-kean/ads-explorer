8e3f0098e1bb0432697ffa0e982a945b223d0d7f
Nate Kean <14845347+garlic-os@users.noreply.github.com>
2024-06-20 00:40:42 -0500

    Add project clean script

---
+++ ADSExplorer/clean.ps1
@@ -0,0 +1,10 @@
+# @pre: pwd is ${workspaceFolder}
+
+if ($null -eq (Get-Command "msbuild.exe" -ErrorAction SilentlyContinue)) {
+    $RealPwd = $pwd
+    Import-Module "C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\Microsoft.VisualStudio.DevShell.dll"
+    Enter-VsDevShell fd4e7f29
+    Set-Location $RealPwd
+}
+
+devenv ADSExplorer.sln /Clean
