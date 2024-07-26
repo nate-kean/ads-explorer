fe81154c14a0139cead2771a18d6ea9835ecad93
Nate Kean <14845347+garlic-os@users.noreply.github.com>
2024-07-26 17:02:38 -0500

    Stop if build fails

--- ADSExplorer/buildandreinstall.ps1
+++ ADSExplorer/buildandreinstall.ps1
@@ -1,4 +1,7 @@
 # @pre: pwd is ${workspaceFolder}
 
 & ((Split-Path $MyInvocation.InvocationName) + "\build.ps1")
+if ($LastExitCode -ne 0) {
+    exit $LastExitCode
+}
 & ((Split-Path $MyInvocation.InvocationName) + "\reinstall.ps1")
