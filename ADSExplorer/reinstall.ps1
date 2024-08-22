a9d0b4df5ebca64cbcb7a6e024acd3dffd211828
Nate <14845347+garlic-os@users.noreply.github.com>
2024-08-22 14:40:31 -0500

    Don't uninstall if not installed
    Added a check whether ADSX is registered before attempting to uninstall it. If it's not installed, skip uninstalling.
    With that, I also added back quitting if uninstallation fails, now that the only valid reason I know of that it could fail has been eliminated.

--- ADSExplorer/reinstall.ps1
+++ ADSExplorer/reinstall.ps1
@@ -1,15 +1,20 @@
 # @pre: pwd is ${workspaceFolder}
 
+$ADSX_REG_PATH = "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Classes\CLSID\{ED383D11-6797-4103-85EF-CBDB8DEB50E2}"
+
 New-Item -ItemType Directory -Force -Path Active\ | Out-Null
 
 taskkill /f /im explorer.exe
 sudo {
-    Push-Location Active\
-        regsvr32 /u ADSExplorer.dll
-        if ($LastExitCode -ne 0) {
-            Write-Warning "Failed to unregister the DLL"
-        }
-    Pop-Location
+    if (Test-Path $ADSX_REG_PATH) {
+        Push-Location Active\
+            regsvr32 /u ADSExplorer.dll
+            if ($LastExitCode -ne 0) {
+                Write-Error "Failed to unregister the DLL"
+                exit $LastExitCode
+            }
+        Pop-Location
+    }
 
     Start-Process cmd.exe `
         -ArgumentList @("/c", "explorer.exe") `
