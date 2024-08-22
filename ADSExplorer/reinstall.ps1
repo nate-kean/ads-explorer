d62ea1e540deacc347e02a08e3cef3a06e8c96c6
Nate <14845347+garlic-os@users.noreply.github.com>
2024-08-22 14:33:52 -0500

    Just call sudo once

--- ADSExplorer/reinstall.ps1
+++ ADSExplorer/reinstall.ps1
@@ -3,22 +3,24 @@
 New-Item -ItemType Directory -Force -Path Active\ | Out-Null
 
 taskkill /f /im explorer.exe
-Push-Location Active\
-    sudo regsvr32 /u ADSExplorer.dll
-    if ($LastExitCode -ne 0) {
-        Start-Process "explorer.exe"
-        Write-Error "Failed to unregister the DLL"
-        exit $LastExitCode
-    }
-Pop-Location
+sudo {
+    Push-Location Active\
+        regsvr32 /u ADSExplorer.dll
+        if ($LastExitCode -ne 0) {
+            Start-Process "explorer.exe"
+            Write-Error "Failed to unregister the DLL"
+            exit $LastExitCode
+        }
+    Pop-Location
 
-Start-Process cmd.exe `
-    -ArgumentList @("/c", "explorer.exe") `
-    -WindowStyle ([System.Diagnostics.ProcessWindowStyle]::Minimized)
+    Start-Process cmd.exe `
+        -ArgumentList @("/c", "explorer.exe") `
+        -WindowStyle ([System.Diagnostics.ProcessWindowStyle]::Minimized)
 
-Copy-Item x64\Debug\* Active\
-Copy-Item ADSExplorer\x64\Debug\ADSExplorer.tlb Active\
+    Copy-Item x64\Debug\* Active\
+    Copy-Item ADSExplorer\x64\Debug\ADSExplorer.tlb Active\
 
-Push-Location Active\
-    sudo regsvr32 ADSExplorer.dll
-Pop-Location
+    Push-Location Active\
+        regsvr32 ADSExplorer.dll
+    Pop-Location
+}
