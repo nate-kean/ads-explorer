24937bfe369f59b1159e790c9e4f5c4a5edefa49
Nate <14845347+garlic-os@users.noreply.github.com>
2024-08-23 13:43:43 -0500

    Refactor

--- Test/buildtestfiles.bat
+++ Test/buildtestfiles.bat
@@ -1,18 +1,15 @@
 ::Pre:: pwd is .\Test\
-
 @echo off
 
 pushd Files
+    mkdir 0streams
 
-mkdir 0streams
+    echo 1stream.txt DATA > 1stream.txt
 
-echo 1stream.txt DATA > 1stream.txt
+    echo 2streams.txt DATA > 2streams.txt
+    echo 2streams.txt alternate1 > 2streams.txt:alternate1
 
-echo 2streams.txt DATA > 2streams.txt
-echo 2streams.txt alternate1 > 2streams.txt:alternate1
-
-echo 3streams.txt DATA > 3streams.txt
-echo 3streams.txt alternate1 > 3streams.txt:alternate1
-echo 3streams.txt alternate2 > 3streams.txt:alternate2
-
+    echo 3streams.txt DATA > 3streams.txt
+    echo 3streams.txt alternate1 > 3streams.txt:alternate1
+    echo 3streams.txt alternate2 > 3streams.txt:alternate2
 popd
