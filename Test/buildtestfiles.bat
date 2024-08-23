35e96945d6519fa2fd7876a6e0143fdd302b174c
Nate <14845347+garlic-os@users.noreply.github.com>
2024-08-23 13:41:58 -0500

    This should run from the test folder

--- Test/buildtestfiles.bat
+++ Test/buildtestfiles.bat
@@ -1,4 +1,9 @@
+::Pre:: pwd is .\Test\
+
 @echo off
+
+pushd Files
+
 mkdir 0streams
 
 echo 1stream.txt DATA > 1stream.txt
@@ -9,3 +14,5 @@ echo 2streams.txt alternate1 > 2streams.txt:alternate1
 echo 3streams.txt DATA > 3streams.txt
 echo 3streams.txt alternate1 > 3streams.txt:alternate1
 echo 3streams.txt alternate2 > 3streams.txt:alternate2
+
+popd
