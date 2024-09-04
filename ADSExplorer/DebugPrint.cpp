0a7c74f08847330612a93662a87495824469e52e
Nate Kean <14845347+garlic-os@users.noreply.github.com>
2024-09-04 18:06:06 -0500

    Pretty print HRESULTs

--- ADSExplorer/DebugPrint.cpp
+++ ADSExplorer/DebugPrint.cpp
@@ -36,8 +36,29 @@ CDebugStream::overflow(CDebugStream::Base::int_type c) {
 // );
 
 #ifdef _DEBUG
+	#include <iomanip>
+	#include <sstream>
+	#include <string>
+
+	static std::wstring HRESULTToString(HRESULT hr) {
+		switch (hr) {
+			case S_OK: return L"S_OK";
+			case S_FALSE: return L"S_FALSE";
+			case E_NOTIMPL: return L"E_NOTIMPL";
+			case E_NOINTERFACE: return L"E_NOINTERFACE";
+			case E_POINTER: return L"E_POINTER";
+			case E_OUTOFMEMORY: return L"E_OUTOFMEMORY";
+			case E_INVALIDARG: return L"E_INVALIDARG";
+			case E_FAIL: return L"E_FAIL";
+			default:
+				std::wstringstream ss;
+				ss << std::hex << std::setw(8) << std::setfill(L'0') << hr;
+				return ss.str();
+		}
+	}
+
 	HRESULT LogReturn(HRESULT hr) {
-		LOG(L" -> " << hr);
+		LOG(L" -> " << HRESULTToString(hr));
 		return hr;
 	}
 #endif
