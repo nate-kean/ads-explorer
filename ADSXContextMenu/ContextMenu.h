058fbc6222f6cf09b228d37cf535009f34a5aed3
Nate <14845347+garlic-os@users.noreply.github.com>
2024-11-12 14:24:39 -0600

    Fill out context menu COM object header

--- ADSXContextMenu/ContextMenu.h
+++ ADSXContextMenu/ContextMenu.h
@@ -13,78 +13,47 @@
 
 
 //==============================================================================
-
-// Set the return string 'Source' in the STRRET struct.
-// Returns false if memory allocation fails.
-bool SetReturnString(_In_ PCWSTR pszSource, _Out_ STRRET *strret);
-
-
-//==============================================================================
 // CADSXContextMenu
 
 class ATL_NO_VTABLE CADSXContextMenu
 	: public CComObjectRootEx<CComSingleThreadModel>,
 	  public CComCoClass<CADSXContextMenu, &CLSID_ADSXContextMenuContextMenu>,
-	  public IShellFolder2,
-	  public IPersistFolder2,
-	  public IShellDetails {
+	  public IExplorerCommand,
+	  public IObjectWithSite {
    public:
-	CADSXContextMenu();
-	virtual ~CADSXContextMenu();
+	// CADSXContextMenu(void);
+	// virtual ~CADSXContextMenu(void);
 
 	DECLARE_REGISTRY_RESOURCEID(IDR_CONTEXTMENU)
 
 	DECLARE_PROTECT_FINAL_CONSTRUCT()
 
 	BEGIN_COM_MAP(CADSXContextMenu)
-		COM_INTERFACE_ENTRY(IShellFolder)
-		COM_INTERFACE_ENTRY(IShellFolder2)
-		COM_INTERFACE_ENTRY(IPersistFolder)
-		COM_INTERFACE_ENTRY(IPersistFolder2)
-		COM_INTERFACE_ENTRY(IPersist)
-		COM_INTERFACE_ENTRY_IID(IID_IShellDetails, IShellDetails)
+		COM_INTERFACE_ENTRY(IExplorerCommand)
+		COM_INTERFACE_ENTRY(IObjectWithSite)
 	END_COM_MAP()
 
 	//--------------------------------------------------------------------------
-	// IPersist
-	STDMETHOD(GetClassID)(_Out_ CLSID*);
+	// IExplorerCommand
+	IFACEMETHOD(GetTitle)(_In_opt_ IShellItemArray* psiaSelection, _Outptr_result_nullonfailure_ PWSTR* ppszName);
+	IFACEMETHOD(GetIcon)(_In_opt_ IShellItemArray* psiaSelection, _Outptr_result_nullonfailure_ PWSTR* ppszIcon);
+	IFACEMETHOD(GetToolTip)(_In_opt_ IShellItemArray* psiaSelection, _Outptr_result_nullonfailure_ PWSTR* ppszInfotip);
+	IFACEMETHOD(GetCanonicalName)(_Out_ GUID* pguidCommandName);
+	IFACEMETHOD(GetState)(_In_opt_ IShellItemArray* psiaSelection, _In_ BOOL bOkToBeSlow, _Out_ EXPCMDSTATE* pfCmdState);
+	IFACEMETHOD(Invoke)(_In_opt_ IShellItemArray* psiaSelection, _In_opt_ IBindCtx* pbc) noexcept;
+	IFACEMETHOD(GetFlags)(_Out_ EXPCMDFLAGS* pFlags);
+	IFACEMETHOD(EnumSubCommands)(_COM_Outptr_ IEnumExplorerCommand** ppEnum);
 
 	//--------------------------------------------------------------------------
-	// IPersistFolder(2)
-	STDMETHOD(Initialize)(_In_ PCIDLIST_ABSOLUTE);
-	STDMETHOD(GetCurFolder)(_Outptr_ PIDLIST_ABSOLUTE*);
-
-	//--------------------------------------------------------------------------
-	// IShellFolder
-	STDMETHOD(BindToObject)(_In_ PCUIDLIST_RELATIVE, _In_opt_ IBindCtx*, _In_ REFIID, _COM_Outptr_ void**);
-	STDMETHOD(CompareIDs)(_In_ LPARAM, _In_ PCUIDLIST_RELATIVE, _In_ PCUIDLIST_RELATIVE);
-	STDMETHOD(CreateViewObject)(_In_ HWND, _In_ REFIID, _COM_Outptr_ void**);
-	STDMETHOD(EnumObjects)(_In_ HWND, _In_ SHCONTF, _COM_Outptr_ IEnumIDList**);
-	STDMETHOD(GetAttributesOf)(_In_ UINT, _In_ PCUITEMID_CHILD_ARRAY, _Inout_ SFGAOF*);
-	STDMETHOD(GetUIObjectOf)(_In_ HWND, _In_ UINT, _In_ PCUITEMID_CHILD_ARRAY, _In_ REFIID, _Inout_ UINT*, _COM_Outptr_ void**);
-	STDMETHOD(BindToStorage)(_In_ PCUIDLIST_RELATIVE, _In_ IBindCtx*, _In_ REFIID, _COM_Outptr_ void**);
-	STDMETHOD(GetDisplayNameOf)(_In_ PCUITEMID_CHILD, _In_ SHGDNF, _Out_ STRRET*);
-	STDMETHOD(ParseDisplayName)(_In_ HWND, _In_opt_ IBindCtx*, _In_ PWSTR, _Out_opt_ ULONG*, _Outptr_ PIDLIST_RELATIVE*, _Inout_opt_ ULONG*);
-	STDMETHOD(SetNameOf)(_In_ HWND, _In_ PCUITEMID_CHILD, _In_ PCWSTR, _In_ SHGDNF, _Outptr_ PITEMID_CHILD*);
-
-	//--------------------------------------------------------------------------
-	// IShellDetails
-	STDMETHOD(ColumnClick)(_In_ UINT);
-	STDMETHOD(GetDetailsOf)(_In_opt_ PCUITEMID_CHILD, _In_ UINT, _Out_ SHELLDETAILS*);
-
-	//--------------------------------------------------------------------------
-	// IShellFolder2
-	STDMETHOD(EnumSearches)(_COM_Outptr_ IEnumExtraSearch**);
-	STDMETHOD(GetDefaultColumn)(_In_ DWORD, _Out_ ULONG*, _Out_ ULONG*);
-	STDMETHOD(GetDefaultColumnState)(_In_ UINT, _Out_ SHCOLSTATEF*);
-	STDMETHOD(GetDefaultSearchGUID)(_Out_ GUID*);
-	STDMETHOD(GetDetailsEx)(_In_ PCUITEMID_CHILD, _In_ const SHCOLUMNID*, _Out_ VARIANT*);
-	// Already in IShellDetails:
-	// STDMETHOD(GetDetailsOf) (PCUITEMID_CHILD, UINT, SHELLDETAILS*);
-	STDMETHOD(MapColumnToSCID)(_In_ UINT, _Out_ SHCOLUMNID*);
+	// IObjectWithSite
+	IFACEMETHOD(SetSite)(_In_opt_ IUnknown* punkSite);
+	_Check_return_ IFACEMETHOD(GetSite)(
+		_In_ REFIID riid,
+		_COM_Outptr_result_maybenull_ void** ppunkSite
+	) noexcept;
 
 	//--------------------------------------------------------------------------
 
    protected:
-	
+	CComPtr<IUnknown> m_punkSite;
 };
