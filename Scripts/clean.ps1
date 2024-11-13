c37d8b23811f0c45f10fff901eee5ad6b51d2eff
Nate <14845347+garlic-os@users.noreply.github.com>
2024-11-13 13:18:58 -0600

    Update clean.ps1

--- Scripts/clean.ps1
+++ Scripts/clean.ps1
@@ -1,6 +1,6 @@
 # @pre: pwd is ${workspaceFolder}
 
-if ($null -eq (Get-Command "msbuild.exe" -ErrorAction SilentlyContinue)) {
+if ($null -eq (Get-Command "devenv.exe" -ErrorAction SilentlyContinue)) {
     $RealPwd = $pwd
     Import-Module "C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\Microsoft.VisualStudio.DevShell.dll"
     Enter-VsDevShell fd4e7f29
