90bec18b1c04b1397d63b681f7d7f95d75d2a8e9
Nate Kean <14845347+garlic-os@users.noreply.github.com>
2024-11-24 20:57:15 -0600

    Actually build the context menu entry
    Oops

--- Scripts/build.ps1
+++ Scripts/build.ps1
@@ -9,15 +9,38 @@ if ($Output.Contains("Build FAILED.")) {
     exit 1
 }
 if (
-    $Output.Contains("Link:`n  All outputs are up-to-date.") -or
-    $Output.Contains("Link:`n  Todas las salidas")
-) {
-    exit 0
-}
-if (
-    !$Output.Contains("Build succeeded.") -and
-    !$Output.Contains(" correcta.")
+    !(
+        $Output.Contains("Build succeeded.") -or
+        $Output.Contains(" correcta.")
+    ) -and !(
+        $Output.Contains("Link:`n  All outputs are up-to-date.") -or
+        $Output.Contains("Link:`n  Todas las salidas")
+    )
 ) {
     Write-Error "Unexpected output from msbuild"
     exit 2
 }
+
+
+. "C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\amd64\MSBuild.exe" `
+    ADSExplorer.sln `
+    /noLogo /t:ADSXContextMenu /p:Configuration=Debug /p:Platform=x64 `
+    | Tee-Object -Variable Output
+$Output = $Output.Replace("`r", "") -join "`n"
+if ($Output.Contains("Build FAILED.")) {
+    exit 1
+}
+if (
+    !(
+        $Output.Contains("Build succeeded.") -or
+        $Output.Contains(" correcta.")
+    ) -and !(
+        $Output.Contains("Link:`n  All outputs are up-to-date.") -or
+        $Output.Contains("Link:`n  Todas las salidas")
+    )
+) {
+    Write-Error "Unexpected output from msbuild"
+    exit 2
+}
+
+exit 0
