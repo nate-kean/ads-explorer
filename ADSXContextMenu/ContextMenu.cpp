f917c6a8ee621f4f8fc31c9b566d27c30644c6f0
Nate Kean <14845347+garlic-os@users.noreply.github.com>
2024-11-24 20:56:14 -0600

    Draw the rest of the owl

--- ADSXContextMenu/ContextMenu.cpp
+++ ADSXContextMenu/ContextMenu.cpp
@@ -5,6 +5,7 @@
  */
 
 #include "StdAfx.h"  // Precompiled header; include first
+#include "debug.h"
 
 #if _MSC_VER > 1200
 	#include "ADSXContextMenu_h.h"
@@ -23,157 +24,161 @@
 
 //==============================================================================
 // CADSXContextMenu
-// CADSXContextMenu::CADSXContextMenu(void) {
-// 	// LOG(P_XCM << L"CONSTRUCTOR");
-// }
-
-
-// CADSXContextMenu::~CADSXContextMenu(void) {
-// 	// LOG(P_XCM << L"DESTRUCTOR");
-// }
-
-
-
-#pragma region IExplorerCommand
-IFACEMETHODIMP CADSXContextMenu::GetTitle(
-	_In_opt_                      IShellItemArray* psiaSelection,
-	_Outptr_result_nullonfailure_ PWSTR*           ppszName
-) {
-	UNREFERENCED_PARAMETER(psiaSelection);
-	LOG(P_XCM << L"GetTitle()");
-	if (ppszName == NULL) return E_POINTER;
-
-	static constexpr PCWSTR pszName = L"Browse alternate data streams";
-	return SHStrDup(pszName, ppszName);
+CADSXContextMenu::CADSXContextMenu(void) : m_pszADSPath((NULL)) {
+	LOG(P_XCM << L"CONSTRUCTOR");
 }
 
-IFACEMETHODIMP CADSXContextMenu::GetIcon(
-	_In_opt_                      IShellItemArray* psiaSelection,
-	_Outptr_result_nullonfailure_ PWSTR*           ppszIcon
-) {
-	UNREFERENCED_PARAMETER(psiaSelection);
-	LOG(P_XCM << L"GetIcon()");
-	if (ppszIcon == NULL) return E_POINTER;
 
-	// std::wstring iconResourcePath = get_module_folderpath(g_hInst);
-	// iconResourcePath += L"\\Assets\\FileLocksmith\\";
-	// iconResourcePath += L"FileLocksmith.ico";
-	// return SHStrDup(iconResourcePath.c_str(), ppszIcon);
-	*ppszIcon = NULL;
-	return E_NOTIMPL;
+CADSXContextMenu::~CADSXContextMenu(void) {
+	LOG(P_XCM << L"DESTRUCTOR");
+	if (m_pszADSPath != NULL) CoTaskMemFree(m_pszADSPath);
 }
 
-IFACEMETHODIMP CADSXContextMenu::GetToolTip(
-	_In_opt_                      IShellItemArray* psiaSelection,
-	_Outptr_result_nullonfailure_ PWSTR*           ppszInfotip
-) {
-	UNREFERENCED_PARAMETER(psiaSelection);
-	LOG(P_XCM << L"GetToolTip()");
-	if (ppszInfotip == NULL) return E_POINTER;
-	*ppszInfotip = NULL;
-	return E_NOTIMPL;
-}
 
-IFACEMETHODIMP
-CADSXContextMenu::GetCanonicalName(_Out_ GUID* pguidCommandName) {
-	LOG(P_XCM << L"GetCanonicalName()");
-	if (pguidCommandName == NULL) return E_POINTER;
-	*pguidCommandName = CLSID_ADSXContextMenu;
-	return S_OK;
-}
-
-IFACEMETHODIMP CADSXContextMenu::GetState(
-	_In_opt_ IShellItemArray* psiaSelection,
-	_In_     BOOL             bOkToBeSlow,
-	_Out_    EXPCMDSTATE*     pfCmdState
+IFACEMETHODIMP CADSXContextMenu::Initialize(
+	_In_opt_ PCIDLIST_ABSOLUTE pidlFolder,
+	_In_     IDataObject*      pdo,
+	_In_     HKEY              hkeyProgID
 ) {
-	UNREFERENCED_PARAMETER(psiaSelection);
-	UNREFERENCED_PARAMETER(bOkToBeSlow);
-	if (pfCmdState == NULL) return E_POINTER;
-	*pfCmdState = ECS_ENABLED;
+	UNREFERENCED_PARAMETER(pidlFolder);
+	UNREFERENCED_PARAMETER(hkeyProgID);
+	LOG(P_XCM << L"Initialize()");
 
 	HRESULT hr;
-	DWORD cpidl;
-	hr = psiaSelection->GetCount(&cpidl);
-	if (FAILED(hr)) return WrapReturn(hr);
+	FORMATETC fmt = { CF_HDROP, NULL, DVASPECT_CONTENT, -1, TYMED_HGLOBAL };
+	STGMEDIUM stg = { TYMED_HGLOBAL };
+ 
+	// Look for CF_HDROP data in the data object. If there
+	// is no such data, return an error back to Explorer.
+	hr = pdo->GetData(&fmt, &stg);
+	if (FAILED(hr)) return WrapReturn(E_INVALIDARG);
+	defer({ ReleaseStgMedium(&stg); });
+	
+	// Get a pointer to the actual data.
+	HDROP hDrop = static_cast<HDROP>(GlobalLock(stg.hGlobal));
+	if (hDrop == NULL) return WrapReturn(E_INVALIDARG);
+	defer({ GlobalUnlock(stg.hGlobal); });
 
-	// Only show context menu for one item.
-	if (cpidl != 1) {
-		*pfCmdState = ECS_HIDDEN;
+	// Sanity check – make sure there is at least one filename.
+	// (0xFFFFFFFF = get file count)
+	UINT uNumFiles = DragQueryFileW(hDrop, 0xFFFFFFFF, NULL, 0);
+	if (uNumFiles == 0) return WrapReturn(E_INVALIDARG);
+	
+	// Get the required size of the first file's name buffer.
+	// (Returned size does not include null terminator)
+	UINT cchPath = DragQueryFileW(hDrop, 0, NULL, 0);
+	PWSTR pszFilePath = new WCHAR[cchPath + 1];
+	defer({ delete[] pszFilePath; });
+
+	// Get the first file's name.
+	UINT uResult = DragQueryFileW(hDrop, 0, pszFilePath, cchPath + 1);
+	if (uResult == 0) return WrapReturn(E_INVALIDARG);
+	LOG(L" ** " << pszFilePath);
+
+	// ADS path = prefix + path
+	size_t cchADSPath = _countof(szPrefix) + cchPath + 1;
+	size_t cbADSPath = cchADSPath * sizeof(WCHAR);
+	m_pszADSPath = static_cast<PWSTR>(CoTaskMemAlloc(cbADSPath));
+	wcsncpy_s(m_pszADSPath, cbADSPath, szPrefix, _countof(szPrefix));
+	wcsncat_s(m_pszADSPath, cbADSPath, pszFilePath, cchPath);
+	
+	return WrapReturn(S_OK);
+}
+
+
+IFACEMETHODIMP CADSXContextMenu::GetCommandString(
+	_In_                 UINT_PTR idCmd,
+	_In_                 UINT     uFlags,
+	_In_                 UINT*    puReserved,
+	_Out_writes_(cchMax) LPSTR    pszName,
+	_In_                 UINT     cchMax
+) {
+	LOG(P_XCM << L"GetCommandString()");
+
+	if (uFlags & GCS_VERBW) {
+		LOG(L" ** GCS_VERBW");
+		// If Explorer is asking for a verb string, and idCmd is the
+		// command associated with our context menu, copy our verb
+		// string into the pszName buffer.
+		if (idCmd == 0) {
+			lstrcpynW(
+				reinterpret_cast<PWSTR>(pszName),
+				L"BrowseADSes",
+				cchMax
+			);
+			return WrapReturn(S_OK);
+		}
+	} else if (uFlags & GCS_HELPTEXT) {
+		LOG(L" ** GCS_HELPTEXT");
+		// If Explorer is asking for a help text string, and idCmd is
+		// the command associated with our context menu, copy our
+		// help text string into the pszName buffer.
+		if (idCmd == 0) {
+			lstrcpynW(
+				reinterpret_cast<PWSTR>(pszName),
+				L"Browse alternate data streams",
+				cchMax
+			);
+			return WrapReturn(S_OK);
+		}
+	}
+	return WrapReturn(E_INVALIDARG);
+}
+
+
+IFACEMETHODIMP CADSXContextMenu::InvokeCommand(
+	_In_ CMINVOKECOMMANDINFO* pici
+) {
+	LOG(P_XCM << L"InvokeCommand()");
+
+	// If lpVerb really points to a string, ignore this function call.
+	// if (HIWORD(pici->lpVerb) != 0) return WrapReturn(E_INVALIDARG);
+	if (!IS_INTRESOURCE((pici->lpVerb))) return WrapReturn(E_INVALIDARG);
+
+	// Get the command index from the low word of lpcmi->lpVerb.
+	UINT uCmd = LOWORD(pici->lpVerb);
+
+	// If the command index is 0, then it's the command associated with our
+	// context menu.
+	if (uCmd != 0) return WrapReturn(E_INVALIDARG);
+
+	ShellExecuteW(
+		pici->hwnd,
+		L"explore",
+		m_pszADSPath,
+		NULL,
+		NULL,
+		SW_SHOWNORMAL
+	);
+
+	return WrapReturn(S_OK);
+}
+
+
+IFACEMETHODIMP CADSXContextMenu::QueryContextMenu(
+	_In_ HMENU hmenu,
+	_In_ UINT  i,
+	_In_ UINT  uidCmdFirst,
+	_In_ UINT  uidCmdLast,
+	_In_ UINT  uFlags
+) {
+	LOG(P_XCM << L"QueryContextMenu()");
+
+	// If the flags include CMF_DEFAULTONLY then we shouldn't do anything.
+	if (uFlags & CMF_DEFAULTONLY) {
+		return WrapReturn(MAKE_HRESULT(SEVERITY_SUCCESS, FACILITY_NULL, 0));
 	}
 
-	return WrapReturn(S_OK);
+	// Use the helper function InsertMenu to add a new menu item.
+	// We specify CMF_SEPARATOR to add a separator.
+	// InsertMenuW(hmenu, i, MF_SEPARATOR | MF_BYPOSITION, 0, NULL);
+	InsertMenuW(
+		hmenu,
+		i,
+		MF_STRING | MF_BYPOSITION,
+		uidCmdFirst,
+		// TODO(garlic-os): put this in a resource string
+		L"Browse alternate data streams"
+	);
+	return WrapReturn(MAKE_HRESULT(SEVERITY_SUCCESS, FACILITY_NULL, 1));
 }
-
-IFACEMETHODIMP CADSXContextMenu::Invoke(
-	_In_opt_ IShellItemArray *psiaSelection,
-	_In_opt_ IBindCtx        *pbc
-) noexcept {
-	UNREFERENCED_PARAMETER(pbc);
-	LOG(P_XCM << L"Invoke()");
-
-	if (psiaSelection == NULL) return WrapReturn(E_POINTER);
-
-	HRESULT hr;
-	DWORD cpidl;
-	hr = psiaSelection->GetCount(&cpidl);
-	if (FAILED(hr)) return WrapReturn(hr);
-	if (cpidl == 0) return WrapReturn(E_INVALIDARG);
-
-	// Only show context menu for one item.
-	// TODO(garlic-os): should this check be here too?
-	if (cpidl > 1) return WrapReturn(E_INVALIDARG);
-
-	IShellItem *psi;
-	hr = psiaSelection->GetItemAt(0, &psi);  // AddRef'd here
-	if (FAILED(hr)) return WrapReturn(hr);
-	defer({ psi->Release(); });
-
-	PWSTR pszFilePath;
-	hr = psi->GetDisplayName(SIGDN_FILESYSPATH, &pszFilePath);
-	if (FAILED(hr)) return WrapReturn(hr);
-	defer({ CoTaskMemFree(pszFilePath); });
-
-	// std::wostringstream ossNewPath;
-	// ossNewPath << g_pszPrefix << pszFilePath;
-	// TODO(garlic-os): browse to new path here
-	// ossNewPath.str();
-
-	return WrapReturn(S_OK);
-}
-
-IFACEMETHODIMP CADSXContextMenu::GetFlags(_Out_ EXPCMDFLAGS *pFlags) {
-	if (pFlags == NULL) return E_POINTER;
-	*pFlags = ECF_DEFAULT;
-	return S_OK;
-}
-IFACEMETHODIMP CADSXContextMenu::EnumSubCommands(
-	_COM_Outptr_ IEnumExplorerCommand **ppEnumCommands
-) {
-	if (ppEnumCommands == NULL) return E_POINTER;
-	*ppEnumCommands = NULL;
-	return E_NOTIMPL;
-}
-
-#pragma endregion
-
-
-//-------------------------------------------------------------------------------
-#pragma region IObjectWithSite
-IFACEMETHODIMP CADSXContextMenu::SetSite(_In_opt_ IUnknown *punkSite) noexcept {
-	m_punkSite = punkSite;
-	return S_OK;
-}
-_Check_return_ IFACEMETHODIMP CADSXContextMenu::GetSite(
-	_In_                          REFIID riid,
-	_COM_Outptr_result_maybenull_ void** ppunkSite
-) noexcept {
-	if (ppunkSite == NULL) return E_POINTER;
-	*ppunkSite = NULL;
-	return m_punkSite.CopyTo(reinterpret_cast<IUnknown**>(ppunkSite));
-	// IObjectWithSite *pObjectWithSite;
-	// HRESULT hr = m_punkSite.QueryInterface(&pObjectWithSite);
-	// if (FAILED(hr)) return hr;
-
-}
-#pragma endregion
